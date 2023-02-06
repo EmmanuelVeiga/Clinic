87c535965c9ee99bc1d32b68981fb74d6ef78696
Emmanuel Veiga <emmanuelveiga@outlook.com>
2023-02-06 17:52:57 -0400

    Correção hosts settings.py

--- gclin/settings.py
+++ gclin/settings.py
@@ -24,11 +24,10 @@ SECRET_KEY = 'django-insecure-*r1=c!&8$7)z$dsw!3zhnzm#y7s@r$dm7rs0te68hq@*#4#7u#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', "chrome-extension://theIDofYourExtension"]
+ALLOWED_HOSTS = ['https://web-production-e1d6.up.railway.app',
+                 "chrome-extension://theIDofYourExtension"]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-e1d6.up.railway.app/'
-]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e1d6.up.railway.app']
 
 # Application definition
 
